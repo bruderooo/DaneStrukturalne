1148ae9a5567624cfbe50102315b23b60538954d
Szymon Jacon <szymon.jacon@allegropay.pl>
2023-03-09 13:21:30 +0100

    **update** main

--- cw1/main.py
+++ cw1/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def split_students(line: str):
     all_students = line.split(" ")[5:]
-    student_indexes = [i-1 for i, word in enumerate(all_students) if word == "nazywa"]
+    student_indexes = [i - 1 for i, word in enumerate(all_students) if word == "nazywa"]
     return [
         all_students[start_index:end_index]
         for start_index, end_index in zip(student_indexes, student_indexes[1:] + [None])
@@ -20,14 +20,16 @@ if __name__ == '__main__':
             splited = line.replace("\n", " ").strip().split()
 
             for student in split_students(line):
-                data.append([
-                    splited[1],
-                    student[0],
-                    student[3],
-                    student[4],
-                    int(student[7]),
-                    [int(oceny) for oceny in student[11:] if oceny not in ("\n", "")],
-                ])
+                data.append(
+                    [
+                        splited[1],
+                        student[0],
+                        student[3],
+                        student[4],
+                        int(student[7]),
+                        [int(oceny) for oceny in student[11:] if oceny not in ("\n", "")],
+                    ]
+                )
 
     df = pd.DataFrame(
         data,
@@ -36,10 +38,19 @@ if __name__ == '__main__':
     df["srednia_ocen"] = df["oceny"].apply(lambda oceny: sum(oceny) / len(oceny))
     grouped_uniwersytet = df.groupby('uniwersytet')
 
-    print(f"Studentów jest {len(df.index)}")
-    print(f"Średnia liczba studentów per uniwersytet {grouped_uniwersytet.count().mean()['id_studenta']}")
-    print(f"Najczęściej imie {df[['imie', 'id_studenta']].groupby('imie').count().sort_values('id_studenta').index[-1]}")
-    print(f"Najrzadziej nazwisko {df[['nazwisko', 'id_studenta']].groupby('nazwisko').count().sort_values('id_studenta').index[0]}")
-    print(f"Sredni wiek {df['wiek'].mean()}")
-    print(f"Srednia ocen {df['srednia_ocen'].mean()}")
-    print(f"Srednia ocen per uniwersytet:\n{grouped_uniwersytet['srednia_ocen'].mean()}")
+    output = "\n".join(
+        (
+            f"Studentów jest {len(df.index)}",
+            f"Średnia liczba studentów per uniwersytet {grouped_uniwersytet.count().mean()['id_studenta']}",
+            f"Najczęściej imie "
+            f"{df[['imie', 'id_studenta']].groupby('imie').count().sort_values('id_studenta').index[-1]}",
+            f"Najrzadziej nazwisko "
+            f"{df[['nazwisko', 'id_studenta']].groupby('nazwisko').count().sort_values('id_studenta').index[0]}",
+            f"Sredni wiek {df['wiek'].mean()}",
+            f"Srednia ocen {df['srednia_ocen'].mean()}",
+            f"Srednia ocen per uniwersytet:\n{grouped_uniwersytet['srednia_ocen'].mean().to_string()}",
+        )
+    )
+
+    with open("output.txt", "w") as f:
+        f.write(output)
