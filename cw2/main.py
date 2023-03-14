ca50ee29d108cf2e99cb3ba202261b508c769851
Szymon Jacon <szymon.jacon@allegropay.pl>
2023-03-14 16:19:07 +0100

    **add** zadanie 2

---
+++ cw2/main.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as et
+from collections import Counter
+from pathlib import Path
+import xml.dom.minidom
+
+
+def dict2xml(dict_data: dict):
+    def elem(data):
+        if isinstance(data, dict):
+            out = ""
+            for key, val in data.items():
+                prefix = "liczba" if isinstance(key, int) else ""
+                sufix =
+                out += f'<{prefix}{key}>{elem(val)}</{prefix}{key}>'
+            return out
+        return data
+
+    return f'<?xml version="1.0"?> <root>{elem(dict_data)}</root>'
+
+
+if __name__ == '__main__':
+    filepath = Path(__file__).parent.joinpath("dl.xml")
+    tree = et.parse(filepath)
+
+    all_nums = (int(elem.text) for elem in tree.iter("numer"))
+    sorted_freq = sorted(Counter(all_nums).items(), key=lambda x: x[1])
+
+    most_common, *_, least_common = sorted_freq
+    out = {
+        "czestotliwosc": dict(sorted_freq),
+        "najczesciej": least_common[0],
+        "najrzadziej": most_common[0],
+    }
+
+    xml_converted = dict2xml(out)
+    dom = xml.dom.minidom.parseString(xml_converted)
+    pretty_xml_as_string = dom.toprettyxml()
+
+    with open("wynik_dl.xml", "w") as f:
+        f.write(pretty_xml_as_string)
